8e0a5e04d92da767bb3d0fb92913c10c67d07b54
Jocsan <jocsancj1703@gmail.com>
2026-03-28 20:54:06 -0600

    Reproductor de pygame, botones de reproducción y detalles

--- GUI Jocsan Calvo Jiménez.py
+++ GUI Jocsan Calvo Jiménez.py
@@ -36,6 +36,17 @@ def imagen_redondeada(ruta, ancho, alto, radio):
     
     return ImageTk.PhotoImage(resultado)
 
+def reproducir(): #Función para reproducir la música de fondo
+    pg.mixer.music.play()
+    pg.mixer.music.unpause()
+
+def pausar(): #Función para pausar la música de fondo
+    pg.mixer.music.pause()
+
+def reiniciar(): #Función para reiniciar la música de fondo
+    pg.mixer.music.stop()
+    pg.mixer.music.play()
+
 
 #Función que ejecuta'Análisis de números'
 def ex_an ():
@@ -124,19 +135,48 @@ def ex_fip ():
     canvafip.create_text(352, 560, text= 'Whyd You Only Call Me When Youre High', font = ('Segoe UI', 10))
     canvafip.create_text(395, 590, text= 'Género musical: Indie Rock', font = ('Segoe UI', 10))
 
+    #Agrega la imagen del programador al canva
     yomero = imagen_redondeada('FotoYo.png', 167, 200, 15)
     canvafip.create_image(120, 210, image=yomero)
     canvafip.image1 = yomero
 
+    #Agrega la imagen del lugar al canva
     lugar = imagen_redondeada('FotoCot.png', 260, 170, 15)
     canvafip.create_image(371, 371, image=lugar)
     canvafip.image2 = lugar
 
+    #Agrega la imagen del artista al canva
     artista = imagen_redondeada('ArticMonkeys.png', 200, 200, 15)
     canvafip.create_image(600, 610, image= artista)
     canvafip.image3 = artista
 
     canvafip.pack()
+
+    pg.mixer.init() #Inicia el mezclador de pygame
+    pg.mixer.music.load('Whyd You Only Call Me When Youre High.mp3')
+
+    #Configura los botones de reproducción, pausa y reinicio de la música
+    marcofip = tk.Frame(canvafip)
+    marcofip.columnconfigure(0, weight=1)
+    marcofip.columnconfigure(1, weight=1)
+    marcofip.columnconfigure(2, weight=1)
+
+    botonrep = tk.Button(marcofip, text='Reproducir', command= lambda: reproducir())
+    botonrep.grid(row=0, column=0)
+
+    botonpau = tk.Button(marcofip, text='Pausar', command= lambda: pausar())
+    botonpau.grid(row=0, column=1)
+
+    botonrei = tk.Button(marcofip, text='Reiniciar', command= lambda: reiniciar())
+    botonrei.grid(row=0, column=2)
+
+    marcofip.place(x= 388, y= 630, anchor='center')
+
+    def cerrar_fip(): #Función para cerrar la ventana de ficha personal y volver al menú principal
+        ventfip.destroy()
+
+    botonmenu = tk.Button(ventfip, text= 'Menú principal', command= lambda: cerrar_fip())
+    botonmenu.place(x= 328, y= 715)
 
 #Configuración de la ventana principal
 ventprincipal = tk.Tk()
