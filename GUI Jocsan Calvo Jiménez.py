580f27b99ab81e67dd393a20ff4cbe7b0c504832
Jocsan <jocsancj1703@gmail.com>
2026-03-28 19:26:13 -0600

    Creación de la ventana y el canva principal

---
+++ GUI Jocsan Calvo Jiménez.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+#Configuración de la ventana principal
+ventprincipal = tk.Tk()
+ventprincipal.title('Tarea')
+ventprincipal.geometry('650x650')
+ventprincipal.resizable(width = False, height = False)
+
+#Configuración del canva principal
+canva1 = tk.Canvas(ventprincipal, bg='#48C0D5')
+canva1.config(width=642, height= 642)
+canva1.create_text(321, 100, text= '¡Bienvenido!', font = ('Segoe UI', 28, 'bold'))
+canva1.pack()
+
+
+ventprincipal.mainloop()
