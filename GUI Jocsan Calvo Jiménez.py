29f1b2020b3a25fb5430a4d7e72a259278a9884e
Jocsan <jocsancj1703@gmail.com>
2026-03-28 19:58:59 -0600

    Se crearon funciones que permiten obtener los pares

--- GUI Jocsan Calvo Jiménez.py
+++ GUI Jocsan Calvo Jiménez.py
@@ -1,4 +1,25 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+#Función asociada a 'Análisis de números'
+def pares(num):
+    if isinstance (num, int) and num > 0:
+        return pares_aux(num, 1)
+                                    #Realmente esta parte de la función no sirve porque la ventana emergente es la que da los errores, pero si se ejecuta sola, sirve
+    else:
+        return 'Digite un  símbolo válido'
+
+def pares_aux(num, op):
+    if op > int(num ** 0.5): #Permite extender la cantidad de números a revisar, ya que tendría un límite de 1000
+        return ""
+    
+    if num % op == 0:
+        return f"({op}, {num//op}) " + pares_aux(num, op + 1)
+
+    else:
+        return pares_aux(num, op + 1)
+    
 
 #Función que ejecuta'Análisis de números'
 def ex_an ():
@@ -16,6 +37,28 @@ def ex_an ():
     canvapares.create_text(321, 110, text= 'Esta función le permitirá hallar los múltiplos de un número entero dado en pares, tal que (a * b) = n', font = ('Segoe UI', 10), width= 530, justify= 'center')
     canvapares.create_text(321, 301, text= 'Digite un número entero: ', font = ('Segoe UI', 10), width= 530, justify= 'left')
     textresult = canvapares.create_text(321, 450, text= '', font= ('Segoe UI', 18, 'bold'), width= 550, justify= 'center' )
+
+
+    def calcular():
+        numero = cnumero.get() #Obtiene el número escrito en el entry
+        
+        if numero == '':
+         messagebox.showerror('Error', 'Digite un número') #Muestra un mensaje de error si el entry está vacío
+         return
+        
+        if not numero.isdigit():
+            messagebox.showerror('Error', 'Solo se permiten números enteros') #Muestra un mensaje de error si se escribe algo que no es un número
+            return
+        
+        if int(numero) > 100000:
+            messagebox.showerror('Error', 'El número es demasiado grande, por favor ingrese un número menor a 100000') #Muestra un mensaje de error si el número es demasiado grande
+            return
+        
+        resultado1 = pares(int(numero))
+        canvapares.itemconfig(textresult, text= 'Los múltiplos del número son: ' + resultado1) #Muestra el resultado en el canva
+
+    cnumero = tk.Entry(ventpares, width= 35)
+    cnumero.place(relx=0.5, rely=0.5, anchor='center')
 
 #Configuración de la ventana principal
 ventprincipal = tk.Tk()
